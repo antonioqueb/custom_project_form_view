67764561a65fefc274cb2c9faf44cefd77f84fa8
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-28 16:30:54 -0600

    Up

--- models/project.py
+++ models/project.py
@@ -1,5 +1,3 @@
-# models/project.py
-
 from odoo import models, fields
 
 class ProjectProject(models.Model):
@@ -17,3 +15,63 @@ class ProjectProject(models.Model):
         string='Scrum Master',
         help='Referencia al usuario asignado como Scrum Master.'
     )
+
+    product_owner = fields.Many2one(
+        'res.users',
+        string='Product Owner',
+        help='Referencia al usuario asignado como Product Owner.'
+    )
+
+    equipo_desarrollo = fields.Many2many(
+        'res.users',
+        string='Equipo de Desarrollo',
+        help='Lista de usuarios asignados como desarrolladores del proyecto.'
+    )
+
+    sprint_actual = fields.Many2one(
+    'project.sprint',
+    string='Sprint Actual',
+    help='Referencia al sprint activo del proyecto.'
+    )
+
+    historias_usuario = fields.Many2many(
+    'project.user_story',
+    string='Historias de Usuario',
+    help='Lista de historias de usuario asociadas al proyecto.'
+    )
+
+    backlog_producto = fields.Many2one(
+    'project.backlog',
+    string='Backlog del Producto',
+    help='Referencia al backlog del producto vinculado al proyecto.'
+    )
+
+    puntos_historia = fields.Integer(
+    string='Puntos de Historia',
+    help='Estimación de puntos de historia para la tarea o proyecto.'
+    )
+
+    estado_sprint = fields.Selection(
+    [('en_progreso', 'En Progreso'), ('finalizada', 'Finalizada'), ('pendiente', 'Pendiente')],
+    string='Estado del Sprint',
+    help='Estado actual de la tarea dentro del sprint.'
+    )
+
+    criterios_aceptacion = fields.Text(
+    string='Criterios de Aceptación',
+    help='Descripción de los criterios de aceptación para la tarea o historia.'
+    )
+
+    revisiones_validaciones = fields.Text(
+    string='Revisiones y Validaciones',
+    help='Información sobre las revisiones y validaciones necesarias.'
+    )
+
+
+
+
+
+
+
+
+
